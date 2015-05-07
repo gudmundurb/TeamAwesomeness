ee76c72e5d21230fe0491e594c2fa0d3e157e075
gudmundurb <gudmundurb14@ru.is>
2015-05-07 12:20:12 +0000

    Meira REPO
    interface og repo

--- SozialSolutions/Sozial/Repositories/GameRepo.cs
+++ SozialSolutions/Sozial/Repositories/GameRepo.cs
@@ -9,6 +9,11 @@ namespace Sozial.Repositories
     public class GameRepo
     {
         private ApplicationDbContext db = new ApplicationDbContext();
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
         //Björn snertir þetta .. ég loka skjali ~geb
     }
 }
