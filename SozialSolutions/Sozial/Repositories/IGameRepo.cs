629836adc2c86bbfa37c78b10caee3e15b377a88
gudmundurb <gudmundurb14@ru.is>
2015-05-07 12:01:07 +0000

    Repos created for GameModel

---
+++ SozialSolutions/Sozial/Repositories/IGameRepo.cs
@@ -0,0 +1,16 @@
+﻿using SozialProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sozial.Repositories
+{
+    public interface IGameRepo : IDisposable
+    {
+     //   public virtual GameModel getGame(int gameID) 
+        //{ 
+         
+       // }
+    }
+}
