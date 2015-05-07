9d4ce47443672fad0743a07206b7681fc40b2dda
Laura <lorsini.franca@gmail.com>
2015-05-07 22:50:17 +0000

    added login box to partial view, css still messed up

--- SozialSolutions/Sozial/Controllers/AccountController.cs
+++ SozialSolutions/Sozial/Controllers/AccountController.cs
@@ -52,6 +52,44 @@ namespace Sozial.Controllers
             }
         }
 
+
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public ActionResult _LoginPartial(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return PartialView();
+        }
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> _LoginPartial(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Login", model);
+            }
+
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, change to shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return RedirectToLocal(returnUrl);
+                case SignInStatus.LockedOut:
+                    return View("Lockout");
+                case SignInStatus.RequiresVerification:
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                    return View(model);
+            }
+        }
+
         //
         // GET: /Account/Login
         [AllowAnonymous]
