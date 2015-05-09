f45ceef8ad0beb0bd535bfc77a5d15466b50a31e
gudmundurb <gudmundurb14@ru.is>
2015-05-09 22:02:38 +0000

    game view modifications

--- SozialSolutions/Sozial/Models/FrontPageViewModel.cs
+++ SozialSolutions/Sozial/Models/FrontPageViewModel.cs
@@ -7,5 +7,11 @@ namespace Sozial.Models
 {
     public class FrontPageViewModel
     {
+        //popular games
+        //recent posts from your friends
+        //
+        //GroupPosts
+        //
+        
     }
 }
