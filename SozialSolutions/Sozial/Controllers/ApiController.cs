bffb8695c2a30e52655af27955e253b78e3bcd89
benedikt <benedikthth@gmail.com>
2015-05-10 21:37:20 +0000

    did some json stuff with playersummary

--- SozialSolutions/Sozial/Controllers/ApiController.cs
+++ SozialSolutions/Sozial/Controllers/ApiController.cs
@@ -5,11 +5,24 @@ using System.Web;
 using System.Web.Mvc;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Configuration;
+using Sozial.Models;
 
 namespace Sozial.Controllers
 {
     public class ApiController : Controller
     {
+
+        public string currUserSteamID()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            string id = (from ApplicationUser n in db.Users
+                        where n.UserName == User.Identity.Name
+                        select n.steamId).Single();
+            return id;
+        }
+
+
         //
         // GET: /Api/
         public async Task<ActionResult> gameList()
@@ -23,5 +36,20 @@ namespace Sozial.Controllers
             }
             
         }
+
+        public async Task<ActionResult> profileInfo(string steamID)
+        {
+            using (var client = new HttpClient()){
+                string apikey = ConfigurationManager.AppSettings["steamApiKey"];
+                string template = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + apikey + "&steamids=" + steamID;
+                var url = new UriBuilder(template);
+                var response = await client.GetAsync(url.ToString());
+                var result = await response.Content.ReadAsStringAsync();
+                return Content(result, "application/json");
+            }
+            
+        }
+
 	}
 }
+
---
+++ SozialSolutions/Sozial/Scripts/playerSumm.js
@@ -0,0 +1,19 @@
+﻿function experiment() {
+    //ask benni before you touch please.
+    //currUserSteamID()
+    var id = "";
+    var url1 = "../Api/currUserSteamID"
+    $.get(url1, null, function (data) {
+        id = data;
+    });
+
+    var url = "../Api/profileInfo/"
+    url = url.concat(id);
+    console.log(url);
+    $.get(url, null, function (data) {
+        var dataaa = data;
+
+        $("#experiment").append(data);
+
+    });
+}
