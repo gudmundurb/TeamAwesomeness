660df9f4be85fa8dffbb4255785e926320b59474
Benedikt Hólm Þórðarson <benedikthth@gmail.com>
2015-05-10 11:44:11 +0000

    did a steam api controller and a little javascript

---
+++ SozialSolutions/Sozial/Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Sozial.Controllers
+{
+    public class ApiController : Controller
+    {
+        //
+        // GET: /Api/
+        public async Task<ActionResult> gameList()
+        {
+            using (var client = new HttpClient())
+            {
+                var url = new UriBuilder("http://api.steampowered.com/ISteamApps/GetAppList/v0001/");
+                var response = await client.GetAsync(url.ToString());
+                var result = await response.Content.ReadAsStringAsync();
+                return Content(result, "application/json");
+            }
+            
+        }
+	}
+}
---
+++ SozialSolutions/Sozial/Scripts/gameList.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    //ask benni before you touch please.
+    var url = "../Api/gameList";
+    $.get(url, null, function (data) {
+        var datalist = data.applist.apps.app;
+        
+
+        $("#steamList").append("<table>");
+
+        $.each(datalist, function (i, item) {
+            //var str = "<tr><td>" + item.name + "</td></tr>";
+            var str = "<tr><td> <a href='Game/Create/" + item.name + "'>" + item.name + "</a></td></tr>";
+            $("#steamList").append(str);
+        })
+
+        $("#steamList").append("</table>");
+    });
+})
