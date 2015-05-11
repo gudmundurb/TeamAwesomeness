4b1b251e4addcd172305ce254e823198421059a2
Laura <lorsini.franca@gmail.com>
2015-05-11 17:55:42 +0000

    More game index stuff

--- SozialSolutions/Sozial/Scripts/text_delimitator.js
+++ SozialSolutions/Sozial/Scripts/text_delimitator.js
@@ -1,12 +1,22 @@
 ﻿$(document).ready(function () {
 
+    var added = false;
+    $('#readMore').hide();
+
     $('.text_limit').each(function ()
     {
-        var myDiv = $('.text_limit');
-
         if ($(this).text().length > 300)
         {
-            $(this).text($(this).text().substring(0, 300) + "(...)");       
+            $(this).text($(this).text().substring(0, 300) + "...");            
+            if (added == false)
+            {
+                $('#readMore').show();
+                added = true;
+            }           
+        }
+        else
+        {
+            $(this).next().css("display", "none");
         }
     });
 });
