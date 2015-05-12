49a3fa2ef1c763cf0569da944c45daf18ba3b97f
gudmundurb <gudmundurb14@ru.is>
2015-05-12 12:20:55 +0000

    rating preparations

---
+++ SozialSolutions/Sozial/Models/RatingModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sozial.Models
+{
+    public class RatingModel
+    {
+        [Key]
+        public int ratingId { get; set; }
+        public int MovieId { get; set; }
+        public string Username { get; set; }
+        public int rating { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
