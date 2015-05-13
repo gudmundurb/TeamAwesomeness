a2d6dadf3bb6db45792dd00c37be19422a9f6533
Laura <lorsini.franca@gmail.com>
2015-05-13 12:12:02 +0000

    Front Page

--- SozialSolutions/Sozial/Scripts/text_delimitator.js
+++ SozialSolutions/Sozial/Scripts/text_delimitator.js
@@ -1,13 +1,21 @@
 ﻿$(document).ready(function () {
 
     var added = false;
+    var pathName = window.location.pathname;
+    var maxSize = 300;
+
+    if (pathName == "/Home/Index" || pathName == "/")
+    {
+        maxSize = 200;
+    }
+
     $('#readMore').hide();
 
     $('.text_limit').each(function ()
     {
-        if ($(this).text().length > 300)
+        if ($(this).text().length > maxSize)
         {
-            $(this).text($(this).text().substring(0, 300) + "...");            
+            $(this).text($(this).text().substring(0, maxSize) + "...");            
             if (added == false)
             {
                 $('#readMore').show();
