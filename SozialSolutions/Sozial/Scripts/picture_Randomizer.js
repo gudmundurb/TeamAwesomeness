3663cbfe392386855467cf5c96550ad030dacdb2
Laura <lorsini.franca@gmail.com>
2015-05-14 01:43:58 +0000

    and, guess what, more front page.

---
+++ SozialSolutions/Sozial/Scripts/picture_Randomizer.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    
+    $('.image_randomizer').each(function () {
+        var images = ['group_bg_1.png', 'group_bg_2.png', 'group_bg_3.png'];
+        $(this).css({ 'background-image': 'url(Content/Images/' + images[Math.floor(Math.random() * images.length)] + ')' });
+
+    });
+});
