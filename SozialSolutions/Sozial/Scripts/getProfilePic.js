78f345d2d5e6dae8e62b9c71416eff524822e4d2
benedikt <benedikthth@gmail.com>
2015-05-15 14:16:15 +0000

    added a loading bar

--- SozialSolutions/Sozial/Scripts/getProfilePic.js
+++ SozialSolutions/Sozial/Scripts/getProfilePic.js
@@ -1,6 +1,10 @@
-﻿function insertProfile () {
+﻿
+function insertProfile() {
     //ask benni before you touch please.
     //currUserSteamID()
+    $("#hideWhenClicked").hide();
+    $("#loader").removeClass("hidden");
+    $("#loader").show();
     var id = "";
     var url = "../Api/profileInfo?steamID="
     var url1 = "../Api/currUserSteamID"
@@ -10,6 +14,9 @@
         url = url.concat(id);
         $.get(url, null, function (data) {
             $("#ProfilePicture").val(data.response.players[0].avatarfull);
+            $("#loader").hide();
+            $("#hideWhenClicked").show();
         });
     });
+    
 }
